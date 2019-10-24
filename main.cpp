15d7d788ef93c432f921347b2d3db522df3b02bb
Ajay Bhaga <ajay.bhaga@clientspectrum.com>
2019-10-24 17:39:13 -0400

    Updated output console message.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+// MayaBrain - built on EANN
+//
+
+#include <iostream>
+#include "random_d.h"
+#include "genotype.h"
+
+int main() {
+    std::cout << "MayaBrain" << std::endl;
+
+    auto genotype = new Genotype();
+
+    std::cout << "Generating random genotype..." << std::endl;
+    genotype = genotype->GenerateRandom(10, 0.4, 20.4);
+    genotype->OutputToConsole();
+    genotype->SaveToFile("genotype01.data");
+    std::cout << "Genotype saved to disk." << std::endl;
+
+    std::cout << "Loading genotype from disk..." << std::endl;
+    genotype = genotype->LoadFromFile("genotype01.data");
+    std::cout << "Loaded genotype from disk." << std::endl;
+
+    return 0;
+}
